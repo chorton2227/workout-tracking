3de0b7e47695b4f170e4d0541c2c2fbe7ccbdb19
Chris Horton <chorton2227@gmail.com>
2014-03-28 22:09:38 -0400

    refactor weight log seeder

--- app/database/seeds/WeightLogsTableSeeder.php
+++ app/database/seeds/WeightLogsTableSeeder.php
@@ -8,17 +8,19 @@ class WeightLogsTableSeeder extends Seeder {
 
 		$user_id = User::where('username', '=', 'user')->first()->id;
 		$weight_logs = array();
-		$start_week = 100;
+		$start_weight = 9.9;
 
-		foreach (range(1, 100) as $index)
+		foreach (range(0, $start_weight - 1, 0.1) as $index)
 		{
-			$current_week = $start_week - $index;
+			$current_weight = $start_weight - $index;
+			$current_day = $current_weight * 10;
+
 			$date = new DateTime();
-			$date->modify("-$current_week week");
+			$date->modify("-$current_day day");
 			
 			$weight_logs[] = array(
 				'user_id' => $user_id,
-				'weight' => 300 - $index,
+				'weight' => $current_weight,
 				'weigh_date' => $date,
 				'notes' => "Note $index",
 				'created_at' => $date,
